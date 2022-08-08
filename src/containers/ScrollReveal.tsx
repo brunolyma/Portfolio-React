2d7b08abc95a9535b4edab47840cff16601f1671
Bruno Lima <brunoadl88@gmail.com>
2022-08-08 17:27:48 -0300

    added feature: scrollReveal

--- src/containers/ScrollReveal.tsx
+++ src/containers/ScrollReveal.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import scrollReveal from "scrollreveal";
 
-export function ScrollReveal({ children, id }: {children: any, id: string}) {
+export function ScrollReveal({ children, id }: {children: ReactNode, id: string}) {
   const sectionRef = useRef<HTMLElement>(null);
 
   useEffect(() => {
     if (sectionRef.current)
       scrollReveal().reveal(sectionRef.current, {
-        reset: true,
+        reset: false,
         delay: 500,
       });
   }, []);
